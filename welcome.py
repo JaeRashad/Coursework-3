ed76a2e045de65218bacff01b029e87db8869131
Timothy Goodman <goodmant@cardiff.ac.uk>
2019-03-17 05:10:24 +0000

    Can now choose time limit when creating test.takeTest method works

--- welcome.py
+++ welcome.py
@@ -1,11 +1,13 @@
 from tkinter import *
 from tkinter import messagebox
 from tkinter import simpledialog
+
 import csv
 import login
 import os
 #import CreateTest
 #Note for later self: check if a test name with the same name exists when creating a test. Maybe also add a timer so it gets deleted automatically
+test_list = []
 
 class Welcome(Frame):
 # GUI Setup
@@ -55,13 +57,15 @@ class Welcome(Frame):
                             modules_list.append(row[i])
         return modules_list
     def retrieveTests(self, module):
+        global test_list # setting it to be global so i can access it within Create_Test
         test_list = []
         with open('tests_overview.csv') as csvfile:
             rdr = csv.reader(csvfile)
             for row in rdr:
                 #print(row[0])
                 if row[0] == module:#Might need to add in the fact that a test gets taken
-                    test_list.append(row[1])
+                    #> row[4] is test duration, adding it to test_list mean the duration vars are more accessible 
+                    test_list.append((row[1], row[4])) 
         if len(test_list) == 0:
             # rather than return empty list, return -1
             return -1
@@ -94,7 +98,7 @@ class Welcome(Frame):
             if test_list != -1:
                 self.listTest.delete(0,END)
                 for test in test_list:
-                    self.listTest.insert(END, test)
+                    self.listTest.insert(END, test[0])
                     self.listTest.selection_set(END)
             else:
                 #clear list box and show message
@@ -110,7 +114,12 @@ class Welcome(Frame):
             import CreateTest
             index = self.listTest.curselection()[0]
             testfile = str(self.listTest.get(index))
+            # Try - Except can be used if neccessary
+            #try:
             CreateTest.Create_Test(t1, testfile+'.csv')
+            #except FileNotFoundError:
+            #    messagebox.showwarning("ERROR", "Test only exists in tests_overview.csv")
+            #    t1.destroy()
         else:
             messagebox.showwarning("ERROR", "Please a pick an existing test to edit.")
     def editTestFast(self, testfile):
@@ -123,35 +132,78 @@ class Welcome(Frame):
         """ This method creates an empty test csv file with a filename specified by the user in a 
             dialog box that appears. It then appends the test's metadata (teacher, testname, module) 
             to the tests_overview.csv file """
-
+        
         if self.listProg.curselection() != ():
             index = self.listProg.curselection()[0]
             strModule = str(self.listProg.get(index))
             #print(strModule)
             name = login.name
             testName = simpledialog.askstring("Input", "Enter test name")
+            #if testname == None or contains forbidden characters 
+            if not testName or len([i for i in testName if i in ['/', '\\', '?', '%', '*', ':', '|', '"', '<', '>', '.']]) != 0:
+                messagebox.showinfo("Error", "You didn't enter a name or you \nused a forbidden character!")
+                return
             testType = simpledialog.askstring("Input", "Formative Test: F, Summative Test: S")
-            # if testName isn't None or "" AND the file doesn't already exist 
-            if testName and os.path.isfile('.\\{}.csv'.format(testName)) == False:
-                if testType.upper() == 'F' or testType.upper() == 'S':
-                    import Test
-                    #t1 = Toplevel()
-                    #t1.title("Test")
-                    Test.test_file(testName, testType.upper(), strModule, name)
-                    print('...Test Created...\n'+72*'-'+'\nTest Name: {0:30}Type: {1:12}Teacher: {2:30}\n'.format(testName, 'Formative' if testType.upper() == 'F' else 'Summative', name))
-                    self.checkTest()
-                    self.editTestFast(testName)
-                else:
-                    messagebox.showwarning("ERROR", "Enter F or S!")
+            if testType and testType.upper() == 'S':
+                testDuration = simpledialog.askinteger("Input", "Enter the test duration time in minutes.\n (Min: 15, Max: 120)")
+                if testDuration < 15:
+                    #> change testDuration to 15 
+                    pass 
+                elif testDuration > 120:
+                    testDuration = 120
+            elif testType and testType.upper() == 'F':
+                #> Formative tests don't need time limits so setting testDuration 
+                #> to be several years is a solution for now
+                testDuration = 'No' # in milliseconds
+            else:
+                messagebox.showwarning("ERROR", "Enter F or S!")
+                return
+            # check if the file already exists in the folder or if testName for the selected module is already in tests_overview
+            if os.path.isfile('.\\{}.csv'.format(testName)) == False and testName not in test_list:
+                import Test
+                #t1 = Toplevel()
+                #t1.title("Test")
+                Test.test_file(testName, testType.upper(), strModule, name, testDuration)
+                print('...Test Created...\n'+120*'-'+'\nTest Name: {0:30}Type: {1:10}Teacher: {2:25}Duration: {3:9}\n'.format(testName, 'Formative' if testType.upper() == 'F' else 'Summative', 
+                    name, 'No time limit' if testType.upper() == 'F' else str(testDuration) + ' minutes'))
+                self.checkTest()
+                self.editTestFast(testName)
+                print(test_list)
             elif testName:
                 messagebox.showwarning("ERROR", "Test with that name already exists!")
+                return
             else:
-                messagebox.showwarning("ERROR", "You must provide a test name")
+                messagebox.showwarning("ERROR", "Something went wrong?!")
+                return
         else:
             messagebox.showwarning("ERROR", "Please select a module!")
+            return
 
     def takeTest(self):
-        pass
+        index = self.listTest.curselection()[0]
+        testName= str(self.listTest.get(index))
+        import shelve
+        #> open the database situated in the test_results folder.
+        #> 
+        #> Retrieve test duration
+        timeLimit = [i[1] for i in test_list if i[0] == testName]
+        print(timeLimit)
+        db = shelve.open("test_results/"+testName+"_results")
+        #check if students ID exists in database, if it returns True then do not allow student to take test if test (if test is summative)
+        try:
+            db[login.username]
+            messagebox.showinfo("Note", "You have already sat this test")
+            db.close()
+        except KeyError:
+            db.close()
+            import TakeTest
+            t1 = Toplevel()
+            t1.geometry("600x500")
+            app = TakeTest.Take_Test(t1, testName, timeLimit, login.username)
+            
+
+
+
 #mainloop
 if login.username != "":
     root = Tk()
