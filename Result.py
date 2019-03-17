94482aa13bff3dfa559c2f74ed94b22ef9f316bb
Timothy Goodman <goodmant@cardiff.ac.uk>
2019-03-17 17:44:47 +0000

    no of attempts now stored too

--- Result.py
+++ Result.py
@@ -2,10 +2,13 @@
 class result(object):
 	""" This class defines a student's results for a test. It will take store student ID which will be the key, and 
 	answers as one list of tuples as the value"""
-	def __init__(self, ID="", answers=[]):
+	def __init__(self, ID="", answers=[], attempts=0):
 		#self.count = count
 		self.ID = ID
 		self.answers = answers
-	
+		self.attempts = attempts
+
 	def toString(self):
-		return f'Test Submitted\nUserID: {self.ID}\nAnswers: {self.answers}'
+		self.attempts+=1
+		return [self.ID, self.attempts, self.answers]
+		
